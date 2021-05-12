142fad90481c8f8759ab929b364f4287f3022ca0
Miguel Gamboa <miguelgamboa@outlook.com>
2021-05-12 20:07:18 +0100

    Support for Records in .net 4.8

---
+++ FireSource/IsExternalInit.cs
@@ -0,0 +1,15 @@
+namespace ClassLibrary {
+    public record Class(string Str) {
+      internal int Int { get; init; }
+    }
+}
+namespace System.Runtime.CompilerServices {
+   using System.ComponentModel;
+   /// <summary>
+   /// Reserved to be used by the compiler for tracking metadata.
+   /// This class should not be used by developers in source code.
+   /// </summary>
+   [EditorBrowsable(EditorBrowsableState.Never)]
+   public static class IsExternalInit {
+   }
+}
